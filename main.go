fa8ee3a8f9bc55fd92da663a8739683af1dda0ce
dzh123xt <dengzhihong3@gmail.com>
2017-08-27 18:12:56 +0800

    up

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	service "github.com/dzh123xt/backing-fulfillment/service"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	server := service.NewServer()
+	server.Run(":" + port)
+
+}
